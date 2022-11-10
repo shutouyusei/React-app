68cb73fc8a7d9b768aa7c5aca7b4946e31da62ab
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2022-11-10 21:28:56 +0900

    [add]入力フォーム

--- src/component/input.jsx
+++ src/component/input.jsx
@@ -1,26 +1,17 @@
 import { React, useState } from "react";
-const T = [];
+
 const Input = () => {
     const [todo, setTodo] = useState([]);
     const [text, setText] = useState("");
-
-    const to_index = () => {
-        console.log({ text });
-        T.push({ text });
-        setTodo(...T, { text });
-        console.log("T", T);
-        console.log({ todo });
-        setText('');
-    }
     const to = () => {
-        console.log({ todo });
+        onsole.log(text),
+            setTodo([...todo, text]),
+            setText('');
     }
     return (
         <div>
             <input type='text' value={text} onChange={(event) => setText(event.target.value)}></input>
-            <input type='button' onClick={to_index}></input>
             <input type='button' onClick={to}></input>
-
         </div >
     )
 }
